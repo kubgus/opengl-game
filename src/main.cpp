cc3c948bce3b3d413ac1f652614681a729b26395
kubgus <ja.gustafik@outlook.sk>
2024-02-02 22:52:18 +0100

    Rectangle :)

--- src/main.cpp
+++ src/main.cpp
@@ -49,22 +49,35 @@ public:
         unsigned int vbo;
         glGenBuffers(1, &vbo);
 
+        unsigned int ebo;
+        glGenBuffers(1, &ebo);
+
         float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
+            0.5f,  0.5f, 0.0f,  
+            0.5f, -0.5f, 0.0f,  
+            -0.5f, -0.5f, 0.0f,  
+            -0.5f,  0.5f, 0.0f   
+        };
+
+        unsigned int indices[] = {
+            0, 1, 3,
+            1, 2, 3,
         };
 
         glBindVertexArray(vao);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
 
         glUseProgram(shader_program);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
     }
 };
 
